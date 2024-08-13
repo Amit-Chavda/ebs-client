ae0ea3c6fdc2e8901c14963bb1fdf454ad1d81ac
Amit <amit.chavda@dronatechlabs.com>
2024-08-13 18:30:57 +0530

    combined login register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
-import { CarouselModule } from '@coreui/angular';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    CarouselModule // Add this
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
